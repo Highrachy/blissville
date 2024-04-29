2680b279cf3805e4a61154775bef2df202557c44
Haruna Popoola <haruna.popoola@yahoo.com>
2024-04-29 04:26:24 +0100

    feature: update investor terms and condition

--- data/investor-terms.js
+++ data/investor-terms.js
@@ -10,122 +10,130 @@ const InvestorTerms = () => {
       </p>
       <ol className="list-unstyled">
         <li>
-          <h6>1. INVESTMENT PLAN</h6>
+          <h6>1. THE PLAN</h6>
           <p>
-            By subscribing to these terms, the investor represents that he/she
-            is interested in investing in THE BLISSVILLE PROJECT and will
-            advance an investment sum to be paid in a single deposit or any
-            other format (to be agreed upon in writing) in Highrachy&apos;s
-            account as detailed below:
+            By subscribing to these terms, the Investment partner represents
+            that he/she is interested in investing in THE BLISSVILLE TERRACES
+            PROJECT and will advance an investment sum to be paid in a single
+            deposit or any other format (to be agreed upon in writing) in
+            Highrachy&apos;s account as detailed below:
           </p>
           <ul className="list-unstyled">
             <li>
-              <strong>Account Name:</strong> Highrachy Investment and Technology
+              <strong>Account Name:</strong> Highrachy Investment & Technology
               Limited
             </li>
             <li>
-              <strong>Account Number:</strong> 0013808391
+              <strong>Account Number:</strong> 0029082860
             </li>
             <li>
-              <strong>Bank:</strong> Guaranty Trust Bank PLC
+              <strong>Bank:</strong> Stanbic IBTC Bank PLC
             </li>
           </ul>
         </li>
         <li>
-          <h6>2. INTEREST</h6>
+          <h6>2. INVESTMENT STRUCTURE</h6>
           <p>
-            <strong>2.1 </strong>The investment sum will accrue interest on
-            principal sum based on Highrachy&apos;s prevailing rates after a
-            duration of 12 (twelve) months. Interest payment is subject to
-            withholding tax.
+            <strong>2.1 </strong>The investment partner shall provide capital to
+            fund the development project as per the agreed investment amount and
+            terms.
           </p>
           <p>
-            <strong>2.2 </strong>Subject to availability of unallocated or
-            unsold units and subject to the approval of Highrachy, the investor
-            may at any time before repayment be entitled to apply to convert
-            his/her investment to one or more unit(s) of flat commensurate with
-            the sum to which the investor is entitled.
-          </p>
-          <p>
-            <strong>2.3 </strong>Upon the exercise of the option set out in
-            Clause 2.1 above, the investor&apos;s expected returns will
-            automatically be exchanged for apartment unit(s) at the current
-            Blissville unit cost on a pro rata basis.
+            <strong>2.2 </strong>The investment shall be considered as equity
+            funding, and Highrachy shall utilize the funds as deemed
+            appropriate.
           </p>
         </li>
         <li>
-          <h6>3. INVESTMENT CONDITIONS</h6>
+          <h6>3. RETURNS AND GUARANTEES</h6>
           <p>
-            <strong>3.1 </strong>The investor is fully aware of and understands
-            the nature of the venture in which he/she is investing as well as
-            the terms contained herein and is fully ready to abide by them.
+            <strong>3.1 </strong> Highrachy estimates an eighteen (18) months
+            window for the return on investment (ROI) realization in line with
+            the project cycle.
           </p>
           <p>
-            <strong>3.2 </strong>Highrachy is and will be at absolute liberty to
-            make decisions concerning the execution of the project, allocation
-            of invested funds, and the appointment of contractors, consultants,
-            and team members without the express permission or influence of the
-            investor.
+            <strong>3.2 </strong> Investment partners have a potential return on
+            investment (ROI) of up to 50% in 18 months (i.e., 33% p.a.).
+            Nonetheless, it is recognized that this ROI is contingent upon the
+            performance of the property development project and may therefore
+            fluctuate.
+          </p>
+          <p>
+            <strong>3.3 </strong> The investment sum will accrue interest on
+            principal sum based on the project&apos;s financial performance at
+            the expiration of the tenure. Interest payment is subject to
+            withholding tax.
+          </p>
+          <p>
+            <strong>3.4 </strong> Subject to availability of unsold units and
+            subject to the approval of Highrachy, the investment partner may at
+            the expiration of the tenure before repayment be entitled to apply
+            to convert his/her investment to one or more unit(s) of flat
+            commensurate with the sum to which the investment partner is
+            entitled.
+          </p>
+          <p>
+            <strong>3.5 </strong> Upon the exercise of the option set out in
+            Clause 3.4 above, the investment partner&apos;s expected returns may
+            be exchanged for apartment unit(s) at the current Blissville unit
+            cost.
+          </p>
+          <p>
+            <strong>3.6 </strong> The investment amount shall be guaranteed by
+            property within the estate giving the investment partner the option
+            to convert his investment plus accrued returns to property ownership
+            by paying the balance in line with the property value.
           </p>
         </li>
         <li>
           <h6>4. TERMINATION</h6>
           <p>
             <strong>4.1 </strong>The investment will be automatically terminated
-            upon the expiration of 12 months from the date of accepting these
-            Terms (subject to deposit of investment sum) and Condition and/or
-            upon full discharge of the investor&apos;s entitlement under this
-            agreement.
+            upon the expiration of the duration for the project from the date of
+            accepting these Terms (subject to deposit of investment sum) and
+            Condition and/or upon full discharge of investment partner&apos;s
+            entitlement under this agreement.
           </p>
           <p>
-            <strong>4.2 </strong>Where the investor wishes to terminate his/her
-            investment before the specified duration of 12 months, the investor
-            will notify Highrachy in writing to which Highrachy will reply
-            within 7 working days and in writing inform the investor of the
-            repayment plan.
+            <strong>4.2 </strong>Where the investment partner wishes to
+            terminate his/her investment before the expiration of the duration
+            for the project, the investment partner will notify Highrachy in
+            writing to which Highrachy will reply within 7 working days and in
+            writing inform the investment partner of the repayment plan.
           </p>
           <p>
-            <strong>4.3 </strong>Where an instruction for termination is not
-            received from the investor at least 24 hours before the maturity
-            date, the Investment will be rolled over at Highrachy&apos;s
-            prevailing interest rates.
+            <strong>4.3 </strong>The investment partner has the option to
+            reinvest the proceeds of the investment in subsequent funding
+            endeavors as will be advised by Highrachy.
           </p>
           <p>
-            <strong>4.4 </strong>Premature termination will attract a penal rate
-            charge which is calculated on a pro-rated basis factoring the
-            outstanding number of days to maturity. This charge will be deducted
-            from the interest earned as per the schedule below:
+            <strong>4.4 </strong>In the event of early redemption (premature
+            termination) that is initiated by the investment partner, the
+            interest payable shall be determined as follows:
           </p>
-          <table className="table table-bordered">
-            <tbody>
-              <tr>
-                <th>Investment Period Lapsed</th>
-                <th>% penalty of interest</th>
-              </tr>
-              <tr>
-                <td> &lt; 6 months</td>
-                <td>100%</td>
-              </tr>
-              <tr>
-                <td>6 Months to &lt; 9 months</td>
-                <td>60%</td>
-              </tr>
-              <tr>
-                <td>9 months to &lt; 12 months</td>
-                <td>25%</td>
-              </tr>
-              <tr>
-                <td>12 months</td>
-                <td>0%</td>
-              </tr>
-            </tbody>
-          </table>
+          <p>
+            <strong>4.4.1 </strong> Less than 6 months - Capital will be
+            refunded in full.
+          </p>
+          <p>
+            <strong>4.4.2 </strong> Between 7-12 months - Capital plus 50% of
+            prevailing FG bond rates.
+          </p>
+          <p>
+            <strong>4.4.3 </strong> Between 13-18 months - the capital plus
+            prevailing FG bond rates will apply.
+          </p>
+          <p>
+            <strong>4.5 </strong> Upon early redemption, the repayment to the
+            investment partner shall be processed solely from the proceeds of
+            property sales.
+          </p>
         </li>
         <li>
           <h6>5. COSTS</h6>
           <p>
-            The investor will bear his/her own costs incurred external to
-            Highrachy in relation to the negotiation, preparation, execution,
+            The investment partner will bear his/her own costs incurred external
+            to Highrachy in relation to the negotiation, preparation, execution,
             performance, amendment, and/or registration of the investment, or
             any consent given or made, and the performance of any action in
             compliance with any obligation or liability arising under any
@@ -135,42 +143,44 @@ const InvestorTerms = () => {
         <li>
           <h6>6. INDEMNITY</h6>
           <p>
-            The investor agrees that he/she/it will indemnify Highrachy and
-            exempt it completely against any proceedings, contention, or legal
-            issue that may arise as a result of the investor&apos;s source of
-            funding or income. Highrachy will in no way be held liable,
-            questionable, or accountable for any reason whatsoever in relation
-            to the source of the investor&apos;s income or invested sum and will
-            not be required to deliver or refund all or any part of the invested
-            sum except in accordance with these terms.
+            The investment partner agrees that he/she/it will indemnify
+            Highrachy and exempt it completely against any proceedings,
+            contention, or legal issue that may arise as a result of the
+            investment partner&apos;s source of funding or income. That
+            Highrachy will in no way be held liable, questionable, or
+            accountable for any reason whatsoever in relation to the source of
+            the investment partner&apos;s income or invested sum and will not be
+            required to deliver or refund all or any part of the invested sum
+            except in accordance with these terms.
           </p>
         </li>
         <li>
           <h6>7. REPRESENTATIONS</h6>
           <p>
-            Highrachy represents to the Investor that at the date of presenting
-            this document and the Subscription Date, Highrachy is a private
-            limited liability company duly established and validly existing
-            under the law of Nigeria that conducts its business in all material
-            respects in accordance with all applicable laws.
+            <strong>7.1</strong> Highrachy represents to the investment partner
+            that at the date of presenting this document and the Subscription
+            Date, Highrachy is a private limited liability company duly
+            established and validly existing under the laws of Nigeria that
+            conducts its business in all material respects in accordance with
+            all applicable laws.
           </p>
           <p>
-            The investor hereby represents to Highrachy that he/she has not
-            directly or indirectly entered into any alliance or agreement that
-            may expose Highrachy to any form of litigation or vicarious
-            responsibility related to the invested sum. The investor further
-            represents that the investment sum deposited by him/her/it is
-            obtained by genuine and honest means.
+            <strong>7.2</strong> The investment partner hereby represents to
+            Highrachy that he/she has not directly or indirectly entered into
+            any alliance or agreement that may tie Highrachy to any form of
+            litigation or vicarious responsibility related to the invested sum.
+            The investment partner further represents that the investment sum
+            deposited by him/her/it is obtained by genuine and honest means.
           </p>
         </li>
         <li>
           <h6>8. NOTICES</h6>
           <p>
-            All notices, which are required to be given, will be in writing or
+            All notices, which are required to be given will be in writing or
             electronic mail and will be sent to the submitted address of the
             recipient. Any such notice may be delivered personally or by courier
             with a copy sent by electronic mail and will be deemed to have been
-            served if by personal delivery, when delivered and if by courier,
+            served if by personal delivery, when delivered, and if by courier,
             upon receipt of proof of delivery.
           </p>
         </li>
@@ -178,7 +188,7 @@ const InvestorTerms = () => {
           <h6>9. EXTENSION</h6>
           <p>
             These terms will be binding on and be for the benefit of the estate,
-            successors, and assignees of the investor.
+            successors, and assignees of the investment partner.
           </p>
         </li>
         <li>
@@ -202,14 +212,14 @@ const InvestorTerms = () => {
             The terms and conditions of this investment arrangement are strictly
             time-bound. Any time or period mentioned in these Terms and
             Conditions may be extended by a written agreement between the
-            investor and Highrachy.
+            investment partner and Highrachy.
           </p>
         </li>
         <li>
           <h6>13. NO PARTNERSHIP</h6>
           <p>
             This is an investment arrangement and will not constitute or be
-            deemed to constitute a partnership between the Parties, and neither
+            deemed to constitute a partnership between the Parties and neither
             of them will have any authority to bind the other in any way except
             as otherwise negotiated and agreed upon in writing in a separate
             document.
@@ -261,15 +271,16 @@ const InvestorTerms = () => {
         <li>
           <h6>18. GOVERNING LAW AND DISPUTE RESOLUTION</h6>
           <p>
-            These Terms will be governed by and construed in accordance with the
-            Laws of the Federal Republic of Nigeria.
+            <strong>18.1</strong> These Terms will be governed by and construed
+            in accordance with the Laws of the Federal Republic of Nigeria.
           </p>
           <p>
-            Any disputes, controversy, or claim arising out of or in relation to
-            or in connection with these Terms and Conditions, will be referred
-            to arbitration after parties have held two meetings with the intent
-            of resolving differences. A dispute will be deemed to have arisen
-            when a Party notifies the other Party in writing to that effect.
+            <strong>18.2</strong> Any disputes, controversy, or claim arising
+            out of or in relation to or in connection with these Terms and
+            Conditions, will be referred to arbitration after parties have held
+            two meetings with the intent of resolving differences. A dispute
+            will be deemed to have arisen when a Party notifies the other Party
+            in writing to that effect.
           </p>
         </li>
       </ol>
