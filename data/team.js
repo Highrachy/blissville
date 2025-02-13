584476cf8dcb43dfa8117c477800fc884d175884
Haruna Popoola <haruna.popoola@yahoo.com>
2025-02-13 08:09:17 +0100

    add omolara to team page

--- data/team.js
+++ data/team.js
@@ -40,6 +40,12 @@ export const team = [
       'The ingenious Haruna manages our ICT and research unit. With a bachelor of technology degree in Computer Science & Engineering and a diploma in Application development, he has constantly proven to be an integral part of such a solution oriented team.',
   },
   {
+    name: 'Omolara Ogbonnaya',
+    image: 'omolara.png',
+    title: 'Sales',
+    description: '-',
+  },
+  {
     name: 'Osasumwen Eghaghe',
     image: 'osas.png',
     title: 'Sales',
