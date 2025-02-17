ceeadff37df43e8d3246b220ce1e3319aea457e7
Haruna Popoola <haruna.popoola@yahoo.com>
2025-02-17 07:55:39 +0100

    add blissville terracees to faqs

--- data/faqs.js
+++ data/faqs.js
@@ -1,92 +1,256 @@
 const faqs = [
   {
-    name: 'About BlissvilleCondominiums',
+    name: 'Blissville Terraces',
     faqs: [
       {
-        question: 'Tell me about Blissville Condominiums',
+        question:
+          'What does Blissville Terraces stand for, and what makes it unique as a property?',
         answer: (
           <>
             <p>
-              Our projects strategically aim at providing energy efficient
-              luxurious condos that are well within your grasp.Unique edifices
-              that don&apos;t just cater for the affordability on procurement
-              but also ensure that the lives of the occupants are enhanced at
-              minimum running and maintenance cost. The architectural designs
-              respond imaginatively to the cultural climatic and environment
-              conditions as such only the most suitable materials are considered
-              and specified. We don&apos;t just sell homes, we guarantee your
-              future.
-            </p>
-            <p>
-              We intend to continuously and progressively expand our portfolio
-              nationwide over the next five years and provide similar housing
-              solutions suitable for strategically selected locations like Ibeju
-              Lekki, Porthharcourt, Uyo, Abuja and Kaduna.,
-            </p>
-          </>
-        ),
-
-        showOnHomePage: true,
-      },
-      {
-        question: <>Where is Blissville located?</>,
-        answer: (
-          <>
-            <p>
-              Blissville Uno is located off Buena vista road by KM 20 within the
-              fast developing community beside the Lekki conservation centre,
-              more easily described as across the Expressway from Chevron. We
-              are accessible via multiple routes giving you the option of choice
-              while on the move.
+              Blissville Terraces stands for lifestyle, quality, comfort, and
+              convenience. It is focused on providing premium, energy-efficient,
+              and affordable housing solutions tailored to customer needs.
             </p>
           </>
         ),
         showOnHomePage: true,
       },
       {
-        question: 'What are the features of Blissville',
+        question: 'What types of units are available in Blissville Terraces?',
         answer: (
-          <div className="px-3">
-            <h4>Safety and Security:</h4>
+          <>
             <p>
-              <strong>&apos;Safety first&apos;</strong>&nbsp;is the watch phrase
-              as we implement several standardized security features;
+              The available unit is a 4-bedroom terraced unit, complete with a
+              home office, a multipurpose room (serving as a home cinema or
+              gym), and a maid&apos;s room.
             </p>
-            <ul>
-              <li>Perimeter fence, electrically protected</li>
-              <li>
-                Automated gates that can be controlled by the push of a button
-              </li>
-              <li>Panic Alarm system</li>
-              <li>Fire detection and firefighting apparatus</li>
-            </ul>
-            <h4>Power and ICT:</h4>
-            <p>
-              We have harnessed superior architectural designs with
-              technological systems that enhance lives and save time and lots of
-              money;
-            </p>
-            <ul>
-              <li>Smart solar and inverter systems</li>
-              <li>Efficient lighting systems</li>
-              <li>Cable TV distribution network</li>
-              <li>Core fiber internet connectivity</li>
-              <li>Intercom and Gate Management</li>
-            </ul>
-            <h4>Ambience and Lifestyle:</h4>
-            <p>
-              We provide plush luxury at affordable rates via a wide range of
-              recreational amenities including;
-            </p>
-            <ul>
-              <li>Children’s play area&nbsp;</li>
-              <li>Rooftop gym/ Dance room</li>
-              <li>Sky lounge with exciting views</li>
-              <li>Dedicated parking for vehicles</li>
-            </ul>
-          </div>
+          </>
         ),
         showOnHomePage: true,
+      },
+      {
+        question: 'Are Blissville Terraces units energy-efficient?',
+        answer: (
+          <>
+            <p>
+              Yes, the units are built with a renewable energy system,
+              energy-efficient appliances, and a strong focus on
+              sustainability—reducing energy consumption by up to 25%.
+            </p>
+          </>
+        ),
+        showOnHomePage: true,
+      },
+      {
+        question: 'Can I customize my unit in Blissville Terraces?',
+        answer: (
+          <>
+            <p>
+              Yes, buyers can customize their units internally with options for
+              changing flooring, paint colors, wall finishes, and other interior
+              features to match personal preferences.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question:
+          'What amenities are available in and around Blissville Terraces?',
+        answer: (
+          <>
+            <p>
+              The units are equipped with modern amenities including CCTV
+              coverage and recreational spaces. Additionally, Blissville
+              Terraces is located near essential services like hospitals,
+              schools, and shopping centers.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question: 'How do I own a unit in Blissville Terraces?',
+        answer: (
+          <>
+            <p>
+              To own a unit, you must apply, execute the purchase agreement, and
+              make full and complete payments.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question:
+          'How is my ownership registered and what legal documents am I entitled to?',
+        answer: (
+          <>
+            <p>
+              Subscribers receive a standard Deed of Assignment—serving as the
+              official document confirming the transfer of ownership. Note that
+              no Deed of Assignment is issued until full payment is completed,
+              after which the Governor&apos;s consent is processed.
+            </p>
+          </>
+        ),
+        showOnHomePage: true,
+      },
+      {
+        question:
+          'Is financing available through banks or developers? (I.e., can I get a mortgage or other financing options to buy a unit?)',
+        answer: (
+          <>
+            <p>
+              Yes, financing is available through banks or the developer,
+              subject to applicable terms and conditions.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question: 'What is the long-term value of the property?',
+        answer: (
+          <>
+            <p>
+              The long-term value is expected to appreciate due to its prime
+              location, energy-efficient features, proximity to key amenities,
+              and the exceptional lifestyle it offers. Market trends and
+              infrastructure development will further influence its value.
+            </p>
+          </>
+        ),
+        showOnHomePage: true,
+      },
+      {
+        question: 'Does Blissville Terraces offer flexible payment plans?',
+        answer: (
+          <>
+            <p>
+              Yes, flexible payment plans tailored to various budget needs,
+              including installment options, are available.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question: `What happens if I can't complete the payment?`,
+        answer: (
+          <>
+            <p>
+              If payment is not completed, penalties may apply and interest will
+              be charged on any outstanding balance. Depending on the purchase
+              agreement, the sale may be canceled. A grace period is provided to
+              remedy the situation; failure to pay within this period may result
+              in rescission of the transaction and refunds processed as per the
+              contract terms.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question: 'Does Blissville Terraces offer home loan services?',
+        answer: (
+          <>
+            <p>No, home loan services are not offered by the developer.</p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question:
+          'Are there hidden charges/fees after purchasing a unit in Blissville Terraces?',
+        answer: (
+          <>
+            <p>
+              There are no hidden charges or agency fees. However, additional
+              costs may arise when the deed is given and the title is
+              transferred after full payment; ownership payments are as outlined
+              in the contract of sale.
+            </p>
+          </>
+        ),
+        showOnHomePage: true,
+      },
+      {
+        question:
+          'When is the delivery and operational date of Blissville Terraces?',
+        answer: (
+          <>
+            <p>The expected delivery and operational date is Q2 2026.</p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question: 'How many units can I own?',
+        answer: (
+          <>
+            <p>
+              Typically, there is no limit on the number of units you can own in
+              Blissville Terraces.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question: 'Is co-ownership an available option?',
+        answer: (
+          <>
+            <p>
+              Yes, co-ownership is available, subject to the Terms and
+              Conditions.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question:
+          'Can I authorize someone else to register my unit by granting them Power of Attorney?',
+        answer: (
+          <>
+            <p>
+              Yes, you can authorize someone to register your unit using a
+              notarized Power of Attorney (PoA) that specifies the property
+              details and tasks. The PoA does not transfer ownership and can be
+              revoked; it should be shared with the Blissville Terraces team for
+              verification.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question:
+          'What is the expected return on investment (ROI) for Blissville Terraces?',
+        answer: (
+          <>
+            <p>
+              The expected ROI is over 40%, with financial investments projected
+              to grow by more than 50% within 18 months.
+            </p>
+          </>
+        ),
+        showOnHomePage: true,
+      },
+      {
+        question: 'When will I start earning returns on my investment?',
+        answer: (
+          <>
+            <p>
+              Returns are expected to begin at the end of the agreed period,
+              projected to be around 18 months, in line with the terms and
+              conditions.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
       },
     ],
   },
@@ -103,15 +267,15 @@ const faqs = [
               &nbsp;for the ever growing middle class within the Lekki suburbs.
               We aim to continually avail this market segment with unique
               edifices that are affordable to acquire and conveniently manage,
-              while they enjoy the luxuries available in today’s real estate
-              industry. Seasoned industry experts diligently working with proven
-              project management methodologies will handle the day to day
+              while they enjoy the luxuries available in today&apos;s real
+              estate industry. Seasoned industry experts diligently working with
+              proven project management methodologies will handle the day to day
               conceptualization, planning, execution and control of the
               projects.
             </p>
             <p>
-              Truth be told, we don’t just sell homes we provide peace of mind
-              to our stakeholders.
+              Truth be told, we don&apos;t just sell homes we provide peace of
+              mind to our stakeholders.
             </p>
             <p>
               We intend to continuously and progressively expand our portfolio
@@ -151,8 +315,99 @@ const faqs = [
             </p>
             <p>These include but are not limited to;</p>
             <ul>
-              <li>Procurement and supply channels</li>{' '}
+              <li>Procurement and supply channels</li>
               <li>Recreational and capacity building services</li>
+            </ul>
+          </div>
+        ),
+        showOnHomePage: false,
+      },
+    ],
+  },
+  {
+    name: 'About BlissvilleCondominiums',
+    faqs: [
+      {
+        question: 'Tell me about Blissville Condominiums',
+        answer: (
+          <>
+            <p>
+              Our projects strategically aim at providing energy efficient
+              luxurious condos that are well within your grasp.Unique edifices
+              that don&apos;t just cater for the affordability on procurement
+              but also ensure that the lives of the occupants are enhanced at
+              minimum running and maintenance cost. The architectural designs
+              respond imaginatively to the cultural climatic and environment
+              conditions as such only the most suitable materials are considered
+              and specified. We don&apos;t just sell homes, we guarantee your
+              future.
+            </p>
+            <p>
+              We intend to continuously and progressively expand our portfolio
+              nationwide over the next five years and provide similar housing
+              solutions suitable for strategically selected locations like Ibeju
+              Lekki, Porthharcourt, Uyo, Abuja and Kaduna.,
+            </p>
+          </>
+        ),
+
+        showOnHomePage: false,
+      },
+      {
+        question: <>Where is Blissville located?</>,
+        answer: (
+          <>
+            <p>
+              Blissville Uno is located off Buena vista road by KM 20 within the
+              fast developing community beside the Lekki conservation centre,
+              more easily described as across the Expressway from Chevron. We
+              are accessible via multiple routes giving you the option of choice
+              while on the move.
+            </p>
+          </>
+        ),
+        showOnHomePage: false,
+      },
+      {
+        question: 'What are the features of Blissville',
+        answer: (
+          <div className="px-3">
+            <h4>Safety and Security:</h4>
+            <p>
+              <strong>&apos;Safety first&apos;</strong>&nbsp;is the watch phrase
+              as we implement several standardized security features;
+            </p>
+            <ul>
+              <li>Perimeter fence, electrically protected</li>
+              <li>
+                Automated gates that can be controlled by the push of a button
+              </li>
+              <li>Panic Alarm system</li>
+              <li>Fire detection and firefighting apparatus</li>
+            </ul>
+            <h4>Power and ICT:</h4>
+            <p>
+              We have harnessed superior architectural designs with
+              technological systems that enhance lives and save time and lots of
+              money;
+            </p>
+            <ul>
+              <li>Smart solar and inverter systems</li>
+              <li>Efficient lighting systems</li>
+              <li>Cable TV distribution network</li>
+              <li>Core fiber internet connectivity</li>
+              <li>Intercom and Gate Management</li>
+            </ul>
+            <h4>Ambience and Lifestyle:</h4>
+            <p>
+              We provide plush luxury at affordable rates via a wide range of
+              recreational amenities including;
+            </p>
+            <ul>
+              <li>Children's play area&nbsp;</li>
+              <li>Rooftop gym/ Dance room</li>
+              <li>Sky lounge with exciting views</li>
+              <li>Dedicated parking for vehicles</li>
             </ul>
           </div>
         ),
